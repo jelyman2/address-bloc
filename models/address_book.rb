c2b131f6c038517ae83cc59deaa9195c1a3f59bf
Johnathan Lyman <jelyman@gmail.com>
2015-11-23 10:34:38 -0800

    Added iterative search method for foundation 22 assignment

--- models/address_book.rb
+++ models/address_book.rb
@@ -46,4 +46,19 @@ class AddressBook
 
     return nil
   end
+
+  def iterative_search(name)
+    # TODO: iterative_search
+    i = 0
+
+    while i <= entries.length - 1
+      if entries[i].name == name
+        return entries[i]
+      else
+        i += 1
+      end
+    end
+
+    return nil
+  end
 end
